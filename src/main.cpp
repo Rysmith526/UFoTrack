0dd3aa5730bc14cd835c3e3b155fb4a3c3a42c20
Nick-Kottas <116681457+Nick-Kottas@users.noreply.github.com>
2024-04-22 15:10:44 -0400

    main.cpp can now test quicksort

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     SightingsDatabase db = SightingsDatabase("ufo_sightings.csv");
-    db.mergeSortByLocation(make_pair(57.5033333, -134.5838889));
+    db.quickSortByLocation(make_pair(57.5033333, -134.5838889));
     for (int i = 0; i < 100; i++)
     {
         cout << db.sightings[i].locationCity << endl;
